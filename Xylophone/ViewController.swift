5b3f9e203bbf7101a9ef0b090ba3bbd97a58b401
faruksirket <farukirket@gmail.com>
2022-12-14 01:28:18 +0300

    The boss challenge is completed with the animation when the key pressed.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,35 @@ class ViewController: UIViewController {
 
 
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.layer.opacity = 0.5
+        playSound(sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+        }
+    }
+    func playSound(_ soundname: String) {
+        guard let url = Bundle.main.url(forResource: soundname, withExtension: "wav") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
